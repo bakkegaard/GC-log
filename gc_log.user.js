e750cf430f5a68fa16ed531e2b6a6501492894c5
Nick Bakkegaard <nickbakkegaard@gmail.com>
2016-11-09 20:10:37 +0800

    rewrote most of it, now it also works for maintenance logs

--- gc_log.user.js
+++ gc_log.user.js
@@ -17,56 +17,106 @@
 	function getURLParameter(name) {
 		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
 	}
+	//See if we got some speciel parameters
+	var type = getURLParameter("type");
+	var date = getURLParameter("date");
+	var comment = getURLParameter("comment");
+
+	//If comments are null set to empty string, then we don't have to check for null everywhere
+	if(comment==null) comment="";
+
+	//If the parameters are not there return
+	if(date==null || type==null) return;
 
 	//Get the current URL
 	var URL = window.location.href;
-	
+
 	//Check if we are on a geocache site
 	if(URL.includes("/geocache/")){
 		//Get an element that only exsist if the cache is logged
-		var element = document.getElementById('ctl00_ContentBody_GeoNav_logText');
-		//If the element is not undefined and the text is found close the window
-		if(element!=undefined && element.textContent=="Found It!"){
-			window.close(); //Close window
+		var element = $("#ctl00_ContentBody_GeoNav_logText");
+
+		//If we already found the cache
+		if(element.html()=="Found It!"){
+			//Close tab
+			window.close();
 		}
-
-		var type= getURLParameter("type");
-		var date= getURLParameter("date");
-
-		if(date==null || type==null) return;
-
 		//Get the logbutton
-		var element = document.getElementById('ctl00_ContentBody_GeoNav_logButton');
-
-		//Get the cache name
-		var el= document.getElementById("ctl00_ContentBody_CoordInfoLinkControl1_uxCoordInfoCode");
-		var gcCode= el.textContent;
+		var log_button = $("#ctl00_ContentBody_GeoNav_logButton");
 
 		//Get url from logbutton
-		var url = element.getAttribute("href");
+		var url = log_button.attr("href");
 
-		//Open a new tab for logging
-		GM_openInTab("http://geocaching.com/"+url+"&type="+type+"&date="+date);
+		//If we have a did not find log and haven't logged it yet
+		if(type=="did not find" && element.html()!="Did Not Find"){
+
+			//Open a log tab
+			GM_openInTab("http://geocaching.com/"+url+"&type="+type+"&date="+date+"&comment="+comment);
+		}
+		else{
+
+			//If comment contains V\space or is just V
+			if(comment.includes("V ") || comment=="V"){
+				//Open a maintenance wet log
+				GM_openInTab("http://geocaching.com/"+url+"&type=maintenance&date="+date+"&comment=wet");
+			}
+			//If comment contains L\space or is just L
+			else if(comment.includes("L ") || comment=="L"){
+				//Open a maintenance logbook log
+				GM_openInTab("http://geocaching.com/"+url+"&type=maintenance&date="+date+"&comment=logbook");
+			}
+
+
+			//Clear comments of L and V
+			comment = comment.replace("V ","").replace("L ","").trim();
+			if(comment=="V" || comment=="L") comment="";
+
+
+			//Open a new tab for logging
+			GM_openInTab("http://geocaching.com/"+url+"&type="+type+"&date="+date+"&comment="+comment);
+		}
 	}
 	//Check if we are on a log site
 	else if(URL.includes("/seek/log")){
 
-		//Get the logtype option
-		var logtype = document.getElementById("ctl00_ContentBody_LogBookPanel1_ddLogType");
-
-		//Set it to found
-		logtype.options[0].removeAttribute("selected");
-		logtype.options[1].setAttribute("selected","selected");
-
-		//Lookup the date in our hashtable
+		//Get parameters
+		var type= getURLParameter("type");
 		var date= getURLParameter("date");
+		var comment= getURLParameter("comment");
 
-		//If it doesnt exsist quit
-		if(date==undefined) return;
+		//If comment is null set it to TFTC!
+		if(comment==null) comment="TFTC!";
 
+		//Return if some of paramters are null
+		if(date==null || type==null || comment==null) return;
+
+		//Get the logtype option
+		var logtype = $("#ctl00_ContentBody_LogBookPanel1_ddLogType");
+
+		//Remove current option selection
+		logtype.val("-1").removeAttr("selected");
+
+		//If it's a found log
+		if(type=="found it"){
+
+			//Select it
+			logtype.val("2").attr("selected","selected");
+		}
+		//Or if it's a maintenance log
+		else if(type=="maintenance"){
+
+			//Select it
+			logtype.val("45").attr("selected","selected");
+
+			//And set log to the appropriate message
+			if(comment=="logbook") comment= "Logbook is full :-)";
+			else if(comment=="wet") comment= "Logbook is wet :-)";
+		}
+
+		//Create a moment object from data
 		date= moment(date);
 
-		//Otherwise make the date string
+		//Make a date String
 		var dateString= (parseInt(date.month())+1)+"/"+ date.date()+"/"+date.year();
 
 		//Set the date
@@ -74,8 +124,9 @@
 		dateField.setAttribute("value",dateString);
 
 		//Get the textField
-		var textField = document.getElementById("ctl00_ContentBody_LogBookPanel1_uxLogInfo");
+		var textField = $("#ctl00_ContentBody_LogBookPanel1_uxLogInfo");
 
 		//Set the logText
-		textField.innerHTML=logtext;
-	}})();
+		textField.html(comment);
+	}
+})();
